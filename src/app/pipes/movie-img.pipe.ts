db1356aa0d31d5a18f9038af22a0edaf37eae292
carlosTangarife <cjt860826@gmail.com>
2017-11-18 19:42:26 -0500

    se crea el pipe de manejo de imagenes movieImg

---
+++ src/app/pipes/movie-img.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'movieImg'
+})
+export class MovieImgPipe implements PipeTransform {
+
+  transform(pelicula: any, poster: boolean = false): any {
+    const url = 'http://image.tmdb.org/t/p/w500';
+
+    if ( poster ) {
+      return url + pelicula.poster_path;
+    }
+
+    if ( pelicula.backdrop_path ) {
+      return url + pelicula.backdrop_path;
+    } else {
+      if ( pelicula.poster_path ) {
+        return url + pelicula.poster_path;
+      } else {
+        return 'assets/img/image-no-available.png';
+      }
+    }
+  }
+
+}
