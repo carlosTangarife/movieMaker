a48876ca6eae0ed9bfd809e9ec99e292e87602e5
carlosTangarife <cjt860826@gmail.com>
2017-11-18 19:41:39 -0500

    se agregan los diccionarios en ingles y español de la aplicacion web

---
+++ src/app/translate/lang-en.ts
@@ -0,0 +1,29 @@
+export const LANG_EN_NAME = 'en';
+
+export const LANG_EN_TRANS = {
+    'hello': 'hello',
+    'seeMore': 'See More',
+    'search': 'Search',
+    'searchMovie': 'Search Movie',
+    'movies': 'Movies',
+    'home': 'Home',
+    'synopsis': 'Synopsis',
+    'famousPhrase': 'Famous phrase',
+    'popularity': 'Popularity',
+    'averageVote': 'Average Vote',
+    'return': 'To return',
+    'logIn': 'log in',
+    'logOut': 'Log Out',
+    'language': 'Language',
+    'english': 'English',
+    'spanish': 'Spanish',
+    'upcomingMovies': 'Upcoming Movies',
+    'popularMovies': 'Popular Movies',
+    'childrenMovies': 'Children\'s Movies',
+    'pleaseLogin': 'Please login',
+    'loginWith': 'Login With',
+    'welcome': 'Welcome to MovieCorner, here you can find all the information of your favorite movies.',
+    'redirect': 'click here, to go to the main page',
+    'go': 'Go!',
+    'notFound': 'Not Found'
+};
---
+++ src/app/translate/lang-es.ts
@@ -0,0 +1,29 @@
+export const LANG_ES_NAME = 'es';
+
+export const LANG_ES_TRANS = {
+    'hello': 'Hola',
+    'seeMore': 'Ver Más',
+    'search': 'Buscar',
+    'searchMovie': 'Buscar Película',
+    'movies': 'Películas',
+    'home': 'Inicio',
+    'synopsis': 'Sinopsis',
+    'famousPhrase': 'Frace Celebre',
+    'popularity': 'Popularidad',
+    'averageVote': 'Voto Promedio',
+    'return': 'Regresar',
+    'logIn': 'Iniciar Sesión',
+    'logOut': 'Cerrar Sesión',
+    'language': 'Idioma',
+    'english': 'Ingles',
+    'spanish': 'Español',
+    'upcomingMovies': 'Películas en Cartelera',
+    'popularMovies': 'Películas Populares',
+    'childrenMovies': 'Películas para Niños',
+    'pleaseLogin': 'Por favor Ingrese',
+    'loginWith': 'Ingresar Con',
+    'welcome': 'Bienvenido a MovieCorner, aqui podras buscar toda la información de tus peliculas favoritas.',
+    'redirect': 'Click aqui, para ir a la pagina principal',
+    'go': 'Ir!',
+    'notFound': 'Pagina no Encontrada'
+};
